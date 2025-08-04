5d27f6b1bbcc26370f413c3c17ea2e5bdccc5c7a
wajaht-ali <contact.chwajahat@gmail.com>
2025-08-04 21:43:37 +0500

    misc: home page editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-red-500'>
-      The Commit: Your code buddie..
+    <div className='w-full h-screen text-blue-500 flex flex-col items-center justify-center'>
+      <p className='text-3xl font-semibold animate-bounce'>Commit: Your AI Code Buddy...</p>
+      <p className='text-2xl font-semibold text-red-500 animate-pulse shadow-sm shadow-red-300'>Comming Soon!</p>
     </div>
   )
 }
