ca4460d113ee46fd180adf26b333444b799314d6
wajaht-ali <contact.chwajahat@gmail.com>
2025-09-01 16:06:03 +0500

    feat: react router integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
+import PageNotFound from "./components/pages/PageNotFound.jsx";
+import Home from "./components/pages/Home.jsx";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='w-full h-screen text-blue-500 flex flex-col items-center justify-center'>
-      <p className='text-3xl font-semibold animate-bounce'>Commit: Your AI Code Buddy...</p>
-      <p className='text-2xl font-semibold text-red-500 animate-pulse shadow-sm shadow-red-300'>Comming Soon!</p>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Not Found */}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
   )
 }
 
