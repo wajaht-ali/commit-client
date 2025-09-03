b37457b423706c3bb16acb848e656774a89477bc
Hammad Ali <hammadali78056@gmail.com>
2025-09-03 21:02:44 +0500

    feat:creating profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
 import './App.css'
+import Profile from './components/Profile/Profile';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='w-full h-screen text-blue-500 flex flex-col items-center justify-center'>
-      <p className='text-3xl font-semibold animate-bounce'>Commit: Your AI Code Buddy...</p>
-      <p className='text-2xl font-semibold text-red-500 animate-pulse shadow-sm shadow-red-300'>Comming Soon!</p>
-    </div>
+    // <div className='w-full h-screen text-blue-500 flex flex-col items-center justify-center'>
+    //   <p className='text-3xl font-semibold animate-bounce'>Commit: Your AI Code Buddy...</p>
+    //   <p className='text-2xl font-semibold text-red-500 animate-pulse shadow-sm shadow-red-300'>Comming Soon!</p>
+    // </div>
+    <>
+<Profile />
+    </>
   )
 }
 
