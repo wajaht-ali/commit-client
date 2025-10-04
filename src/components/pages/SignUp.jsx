c19f31c4dcb8b0204f7d7b814ad6c3ee33049585
Hammad Ali <hammadali78056@gmail.com>
2025-10-04 21:13:27 +0500

    fix: changes in signup page

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 
+
 function SignUp() {
+  
   return (
     <div className='relative min-h-screen flex flex-row'>
-    <div className=" w-full  justify-center items-center h-screen hidden md:flex">
+    <div className=" w-full-2/3  justify-center items-center h-screen hidden md:flex">
         <img src="https://images.unsplash.com/photo-1526925539332-aa3b66e35444?q=80&w=1065&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="login side image" className="object-cover w-full h-full" />
       </div>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 w-full">
-        <h1 className="text-3xl font-bold">Wellcome To Commit</h1>
+        <h1 className="text-3xl font-bold">Register Your Account</h1>
         <form className="flex flex-col space-y-4 mt-4">
+        <input
+            type="text"
+            placeholder="Name"
+            className="px-4 py-2 border border-gray-300 rounded"
+          /><input
+            type="email"
+            placeholder="Email"
+            className="px-4 py-2 border border-gray-300 rounded"
+          />
           <input
             type="text"
             placeholder="Username"
@@ -19,10 +30,16 @@ function SignUp() {
             placeholder="Password"
             className="px-4 py-2 border border-gray-300 rounded"
           />
+          <input
+            type='checkbox'
+            className="h-4 w-4"
+            />
+          <span className="text-sm">I agree to the <a href="#" className="text-blue-500">Terms and Conditions</a></span>
           <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded">
             Sign Up
           </button>
         </form>
+        
         <div className="mt-4">
           <p className="text-sm">
             Already have an account? <a href="/login" className="text-blue-500">Login</a>
